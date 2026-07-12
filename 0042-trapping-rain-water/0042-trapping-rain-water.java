ba6a088833ac8c55b4b0b5873b790f2f49d87240
Abhas Verma <abhasverma111@gmail.com>
2026-07-13 01:08:11 +0530

    Update 0042-trapping-rain-water.java

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -2,7 +2,7 @@ class Solution {
     public int trap(int[] height) {
         int left = 0;
         int right = height.length - 1;
-        int leftMax = 0;
+        int leftMax = 0;   
         int rightMax = 0;
         int totalWater = 0;
         while (left < right) {
