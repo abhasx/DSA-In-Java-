b8dbb0c79d1f77d57384e10fcf0ae4a25cddf4c8
Abhas Verma <abhasverma111@gmail.com>
2026-08-20 22:01:58 +0530

    Time: 2 ms (87.08%), Space: 49.6 MB (13.57%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -1,43 +1,34 @@
 class Solution {
     public void reorderList(ListNode head) {
-
-        if (head == null || head.next == null) {
+        if(head==null||head.next==null){
             return;
+        }   
+        ListNode fast=head,slow=head;
+        //divide list into two halves
+        while(fast.next!=null&&fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        ListNode current=slow.next;
+        slow.next=null;
+        //reverse the second list
+        ListNode prev=null;
+        while(current!=null){
+        ListNode next=current.next;
+        current.next=prev;
+        prev=current;
+        current=next;
         }
-
-        ListNode prev = null;
-        ListNode curr = slow.next;
-
-        slow.next = null;
-
-        while (curr != null) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-
-    
-        ListNode first = head;
-        ListNode second = prev;
-
-        while (second != null) {
-            ListNode firstNext = first.next;
-            ListNode secondNext = second.next;
-
-            first.next = second;
-            second.next = firstNext;
-
-            first = firstNext;
-            second = secondNext;
+        //merge two linklist
+        ListNode first=head;
+        ListNode second=prev;
+        while(second!=null){
+            ListNode firstNext=first.next;
+            ListNode secondNext=second.next;
+            first.next=second;
+            second.next=firstNext;
+            first=firstNext;
+            second=secondNext;
         }
     }
 }
